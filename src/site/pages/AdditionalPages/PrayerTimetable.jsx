5442eaa2672f4d1a612883ab7894dfb4c3d1a76d
Sadia492 <sadiaafrin2792>
2025-03-22 20:33:04 +0600

    220325 prayer timetable page modified

--- src/site/pages/AdditionalPages/PrayerTimetable.jsx
+++ src/site/pages/AdditionalPages/PrayerTimetable.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import CmnBanner from "../../sharedComponents/CmnBanner";
+import PrayerComp from "../../components/AdditionalPages/PrayerComp";
 
 export default function PrayerTimetable() {
   return (
     <div>
       <CmnBanner title="Prayer Timetable"></CmnBanner>
+      <PrayerComp></PrayerComp>
     </div>
   );
 }
