1a0c19447da9fcd4020af01a6d8d2ed728678f58
Sadia492 <sadiaafrin2792>
2025-04-17 18:44:29 +0600

    170425 baseurl stored in env

--- src/redux/features/api/apiSlice.js
+++ src/redux/features/api/apiSlice.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://35.176.169.111",
+    baseUrl: import.meta.env.VITE_WebApi_URL,
   }),
   tagTypes: [],
   endpoints: (builder) => ({}),
