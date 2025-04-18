da4ec9da798aef3cff1389da58985e50fc3b33f3
Sadia492 <sadiaafrin2792>
2025-04-18 21:35:54 +0600

    180425 i18n configuration fixed

--- src/site/i18n.js
+++ src/site/i18n.js
@@ -3,9 +3,25 @@ import LanguageDetector from "i18next-browser-languagedetector";
 import { initReactI18next } from "react-i18next";
 import Backend from "i18next-http-backend";
 
-i18n.use(LanguageDetector).use(initReactI18next).use(Backend).init({
-  debug: true,
-  lng: "en",
-  fallbackLng: "en",
-  returnObjects: true,
-});
+// i18n.use(LanguageDetector).use(initReactI18next).use(Backend).init({
+//   debug: true,
+//   lng: "en",
+//   fallbackLng: "en",
+//   returnObjects: true,
+// });
+
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .use(Backend)
+  .init({
+    debug: true,
+    lng: "en",
+    fallbackLng: "en",
+    returnObjects: true,
+    backend: {
+      loadPath: "/locales/{{lng}}/{{ns}}.json", // <== This part is important!
+    },
+    ns: ["common", "home"], // list all namespaces (files)
+    defaultNS: "common", // default one to fall back to
+  });
