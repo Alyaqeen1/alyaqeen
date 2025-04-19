dafb933f335e768de5668d1225777eff4917567a
Sadia492 <sadiaafrin2792>
2025-04-19 20:02:46 +0600

    190425 baseurl fixed

--- src/redux/features/api/apiSlice.js
+++ src/redux/features/api/apiSlice.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://35.176.169.111",
+    baseUrl: "https://35.176.169.111",
     // baseUrl: "https://talibiq.com",
     // baseUrl: import.meta.env.VITE_WebApi_URL,
   }),
