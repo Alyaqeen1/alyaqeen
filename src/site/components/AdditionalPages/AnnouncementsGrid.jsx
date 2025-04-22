458d9689795c0092835b8928fab4c58dac9cfb03
Sadia492 <sadiaafrin2792>
2025-04-22 19:35:21 +0600

    220425 announcements grid updated

--- src/site/components/AdditionalPages/AnnouncementsGrid.jsx
+++ src/site/components/AdditionalPages/AnnouncementsGrid.jsx
@@ -5,380 +5,99 @@ import three from "../../assets/img/news/06.jpg";
 import four from "../../assets/img/news/08.jpg";
 import five from "../../assets/img/news/09.jpg";
 import six from "../../assets/img/news/10.jpg";
+import { useGetAnnouncementsQuery } from "../../../redux/features/announcements/announcementsApi";
+import LoadingSpinner from "../LoadingSpinner";
+import { useState } from "react";
 
 const AnnouncementsGrid = () => {
+  const {
+    data: announcements,
+    isLoading,
+    isError,
+  } = useGetAnnouncementsQuery();
+
+  const [expandedIds, setExpandedIds] = useState([]);
+
+  const toggleReadMore = (id) => {
+    setExpandedIds((prev) =>
+      prev.includes(id) ? prev.filter((item) => item !== id) : [...prev, id]
+    );
+  };
+
+  const isClamped = (text) => {
+    return text?.length > 100; // Roughly 2 lines
+  };
+
+  if (isLoading) return <LoadingSpinner />;
+  if (isError) return <h2 className="text-center my-4">Error loading news</h2>;
+
   return (
     <section className="news-section-3 fix section-padding">
       <div className="container">
         <div className="row g-4">
-          <div
-            className="col-xl-4 col-lg-6 col-md-6 "
-            data-aos-duration="800"
-            data-aos="fade-up"
-            data-aos-delay="300"
-          >
-            <div className="news-card-items mt-0">
-              <div className="news-image">
-                <img src={one} alt="img" />
-                <div className="news-layer-wrapper">
-                  <div
-                    className="news-layer-image"
-                    style={{
-                      backgroundImage: "url(/img/news/04.jpg)",
-                    }}
-                  ></div>
-                  <div
-                    className="news-layer-image"
-                    style={{
-                      backgroundImage: "url(/img/news/04.jpg)",
-                    }}
-                  ></div>
-                  <div
-                    className="news-layer-image"
-                    style={{
-                      backgroundImage: "url(/img/news/04.jpg)",
-                    }}
-                  ></div>
-                  <div
-                    className="news-layer-image"
-                    style={{
-                      backgroundImage: "url(/img/news/04.jpg)",
-                    }}
-                  ></div>
+          {announcements?.data?.length > 0 ? (
+            announcements.data.map((announcement) => {
+              const isExpanded = expandedIds.includes(announcement.id);
+              const showReadMore = isClamped(announcement.description);
+
+              return (
+                <div
+                  key={announcement?.id}
+                  className="col-xl-4 col-lg-6 col-md-6 d-flex"
+                  data-aos-duration="800"
+                  data-aos="fade-up"
+                  data-aos-delay="300"
+                >
+                  <div className="news-card-items mt-0 d-flex flex-column w-100">
+                    <div className="news-image">
+                      <div className="post">
+                        <span>Activities</span>
+                      </div>
+                    </div>
+                    <div className="news-content">
+                      <ul>
+                        <li>
+                          <i className="fas fa-calendar-alt"></i>
+                          {new Date(
+                            announcement?.created_at
+                          ).toLocaleDateString("en-US", {
+                            year: "numeric",
+                            month: "long",
+                            day: "numeric",
+                          })}
+                        </li>
+                        <li>
+                          <i className="far fa-user"></i>
+                          By admin
+                        </li>
+                      </ul>
+                      <h3>{announcement?.title}</h3>
+
+                      <h6
+                        className={`description ${
+                          !isExpanded ? "clamp-2-lines" : ""
+                        }`}
+                      >
+                        {announcement?.description}
+                      </h6>
+
+                      {showReadMore && (
+                        <button
+                          className="theme-btn-2 mt-3 d-flex align-items-center"
+                          onClick={() => toggleReadMore(announcement.id)}
+                        >
+                          {isExpanded ? "Show Less" : "Read More"}
+                          <i className="fas fa-long-arrow-right ms-2"></i>
+                        </button>
+                      )}
+                    </div>
+                  </div>
                 </div>
-                <div className="post">
-                  <span>Activities</span>
-                </div>
-              </div>
-              <div className="news-content">
-                <ul>
-                  <li>
-                    <i className="fas fa-calendar-alt"></i>
-                    Feb 10, 2024
-                  </li>
-                  <li>
-                    <i className="far fa-user"></i>
-                    By admin
-                  </li>
-                </ul>
-                <h3>
-                  <Link to="news-details">
-                    From without content <br />
-                    style without
-                  </Link>
-                </h3>
-                <Link to="news-details" className="theme-btn-2 mt-3">
-                  Read More <i className="fas fa-long-arrow-right"></i>
-                </Link>
-              </div>
-            </div>
-          </div>
-          <div
-            className="col-xl-4 col-lg-6 col-md-6 "
-            data-aos-duration="800"
-            data-aos="fade-up"
-            data-aos-delay="500"
-          >
-            <div className="news-card-items mt-0">
-              <div className="news-image">
-                <img src={two} alt="img" />
-                <div className="news-layer-wrapper">
-                  <div
-                    className="news-layer-image"
-                    style={{
-                      backgroundImage: "url(/img/news/05.jpg)",
-                    }}
-                  ></div>
-                  <div
-                    className="news-layer-image"
-                    style={{
-                      backgroundImage: "url(/img/news/05.jpg)",
-                    }}
-                  ></div>
-                  <div
-                    className="news-layer-image"
-                    style={{
-                      backgroundImage: "url(/img/news/05.jpg)",
-                    }}
-                  ></div>
-                  <div
-                    className="news-layer-image"
-                    style={{
-                      backgroundImage: "url(/img/news/05.jpg)",
-                    }}
-                  ></div>
-                </div>
-                <div className="post">
-                  <span>Activities</span>
-                </div>
-              </div>
-              <div className="news-content">
-                <ul>
-                  <li>
-                    <i className="fas fa-calendar-alt"></i>
-                    Feb 10, 2024
-                  </li>
-                  <li>
-                    <i className="far fa-user"></i>
-                    By admin
-                  </li>
-                </ul>
-                <h3>
-                  <Link to="news-details">
-                    That jerk Form Finance <br />
-                    really threw me
-                  </Link>
-                </h3>
-                <Link to="news-details" className="theme-btn-2 mt-3">
-                  Read More <i className="fas fa-long-arrow-right"></i>
-                </Link>
-              </div>
-            </div>
-          </div>
-          <div
-            className="col-xl-4 col-lg-6 col-md-6 "
-            data-aos-duration="800"
-            data-aos="fade-up"
-            data-aos-delay="700"
-          >
-            <div className="news-card-items mt-0">
-              <div className="news-image">
-                <img src={three} alt="img" />
-                <div className="news-layer-wrapper">
-                  <div
-                    className="news-layer-image"
-                    style={{
-                      backgroundImage: "url(/img/news/06.jpg)",
-                    }}
-                  ></div>
-                  <div
-                    className="news-layer-image"
-                    style={{
-                      backgroundImage: "url(/img/news/06.jpg)",
-                    }}
-                  ></div>
-                  <div
-                    className="news-layer-image"
-                    style={{
-                      backgroundImage: "url(/img/news/06.jpg)",
-                    }}
-                  ></div>
-                  <div
-                    className="news-layer-image"
-                    style={{
-                      backgroundImage: "url(/img/news/06.jpg)",
-                    }}
-                  ></div>
-                </div>
-                <div className="post">
-                  <span>Activities</span>
-                </div>
-              </div>
-              <div className="news-content">
-                <ul>
-                  <li>
-                    <i className="fas fa-calendar-alt"></i>
-                    Feb 10, 2024
-                  </li>
-                  <li>
-                    <i className="far fa-user"></i>
-                    By admin
-                  </li>
-                </ul>
-                <h3>
-                  <Link to="news-details">
-                    All Inclusive Ultimate Circle <br /> Island Day with Lunch
-                  </Link>
-                </h3>
-                <Link to="news-details" className="theme-btn-2 mt-3">
-                  Read More <i className="fas fa-long-arrow-right"></i>
-                </Link>
-              </div>
-            </div>
-          </div>
-          <div
-            className="col-xl-4 col-lg-6 col-md-6 "
-            data-aos-duration="800"
-            data-aos="fade-up"
-            data-aos-delay="300"
-          >
-            <div className="news-card-items mt-0">
-              <div className="news-image">
-                <img src={four} alt="img" />
-                <div className="news-layer-wrapper">
-                  <div
-                    className="news-layer-image"
-                    style={{
-                      backgroundImage: "url(/img/news/08.jpg)",
-                    }}
-                  ></div>
-                  <div
-                    className="news-layer-image"
-                    style={{
-                      backgroundImage: "url(/img/news/08.jpg)",
-                    }}
-                  ></div>
-                  <div
-                    className="news-layer-image"
-                    style={{
-                      backgroundImage: "url(/img/news/08.jpg)",
-                    }}
-                  ></div>
-                  <div
-                    className="news-layer-image"
-                    style={{
-                      backgroundImage: "url(/img/news/08.jpg)",
-                    }}
-                  ></div>
-                </div>
-                <div className="post">
-                  <span>Activities</span>
-                </div>
-              </div>
-              <div className="news-content">
-                <ul>
-                  <li>
-                    <i className="fas fa-calendar-alt"></i>
-                    Feb 10, 2024
-                  </li>
-                  <li>
-                    <i className="far fa-user"></i>
-                    By admin
-                  </li>
-                </ul>
-                <h3>
-                  <Link to="news-details">
-                    That jerk Form Finance really threw me
-                  </Link>
-                </h3>
-                <Link to="news-details" className="theme-btn-2 mt-3">
-                  Read More <i className="fas fa-long-arrow-right"></i>
-                </Link>
-              </div>
-            </div>
-          </div>
-          <div
-            className="col-xl-4 col-lg-6 col-md-6 "
-            data-aos-duration="800"
-            data-aos="fade-up"
-            data-aos-delay="500"
-          >
-            <div className="news-card-items mt-0">
-              <div className="news-image">
-                <img src={five} alt="img" />
-                <div className="news-layer-wrapper">
-                  <div
-                    className="news-layer-image"
-                    style={{
-                      backgroundImage: "url(/img/news/09.jpg)",
-                    }}
-                  ></div>
-                  <div
-                    className="news-layer-image"
-                    style={{
-                      backgroundImage: "url(/img/news/09.jpg)",
-                    }}
-                  ></div>
-                  <div
-                    className="news-layer-image"
-                    style={{
-                      backgroundImage: "url(/img/news/09.jpg)",
-                    }}
-                  ></div>
-                  <div
-                    className="news-layer-image"
-                    style={{
-                      backgroundImage: "url(/img/news/09.jpg)",
-                    }}
-                  ></div>
-                </div>
-                <div className="post">
-                  <span>Activities</span>
-                </div>
-              </div>
-              <div className="news-content">
-                <ul>
-                  <li>
-                    <i className="fas fa-calendar-alt"></i>
-                    Feb 10, 2024
-                  </li>
-                  <li>
-                    <i className="far fa-user"></i>
-                    By admin
-                  </li>
-                </ul>
-                <h3>
-                  <Link to="news-details">
-                    How to Keep Children Safe Online In Simple Steps
-                  </Link>
-                </h3>
-                <Link to="news-details" className="theme-btn-2 mt-3">
-                  Read More <i className="fas fa-long-arrow-right"></i>
-                </Link>
-              </div>
-            </div>
-          </div>
-          <div
-            className="col-xl-4 col-lg-6 col-md-6 "
-            data-aos-duration="800"
-            data-aos="fade-up"
-            data-aos-delay="700"
-          >
-            <div className="news-card-items mt-0">
-              <div className="news-image">
-                <img src={six} alt="img" />
-                <div className="news-layer-wrapper">
-                  <div
-                    className="news-layer-image"
-                    style={{
-                      backgroundImage: "url(/img/news/10.jpg)",
-                    }}
-                  ></div>
-                  <div
-                    className="news-layer-image"
-                    style={{
-                      backgroundImage: "url(/img/news/10.jpg)",
-                    }}
-                  ></div>
-                  <div
-                    className="news-layer-image"
-                    style={{
-                      backgroundImage: "url(/img/news/10.jpg)",
-                    }}
-                  ></div>
-                  <div
-                    className="news-layer-image"
-                    style={{
-                      backgroundImage: "url(/img/news/10.jpg)",
-                    }}
-                  ></div>
-                </div>
-                <div className="post">
-                  <span>Activities</span>
-                </div>
-              </div>
-              <div className="news-content">
-                <ul>
-                  <li>
-                    <i className="fas fa-calendar-alt"></i>
-                    Feb 10, 2024
-                  </li>
-                  <li>
-                    <i className="far fa-user"></i>
-                    By admin
-                  </li>
-                </ul>
-                <h3>
-                  <Link to="news-details">
-                    Students Intelligence in Education Building Resilient
-                  </Link>
-                </h3>
-                <Link to="news-details" className="theme-btn-2 mt-3">
-                  Read More <i className="fas fa-long-arrow-right"></i>
-                </Link>
-              </div>
-            </div>
-          </div>
+              );
+            })
+          ) : (
+            <h2 className="text-center my-4">No Announcements Available</h2>
+          )}
         </div>
       </div>
     </section>
