411bb4fd066c959c9f900f5b8fa398c09b15be3f
Sadia492 <sadiaafrin2792>
2025-08-12 14:00:43 +0600

    position fixed

--- src/dashboard/components/Sidebar/OffCanvasMenu.jsx
+++ src/dashboard/components/Sidebar/OffCanvasMenu.jsx
@@ -162,14 +162,15 @@ export default function OffCanvasMenu() {
                         identifier="students"
                         submenuItems={[
                           { label: "Add New", to: "add-student" },
-                          {
-                            label: "Online Admission",
-                            to: "online-admissions",
-                          },
+
                           { label: "Active Students", to: "active-students" },
                           {
                             label: "Inactive Students",
                             to: "inactive-students",
+                          },
+                          {
+                            label: "Online Admission",
+                            to: "online-admissions",
                           },
                         ]}
                         openSubMenu={openSubMenu}
