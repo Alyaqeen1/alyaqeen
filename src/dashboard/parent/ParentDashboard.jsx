4b4f6711686fd53e47fd427997dbffdeb25a4705
Sadia492 <sadiaafrin2792@gmail.com>
2025-09-27 22:21:41 +0600

    test completed

--- src/dashboard/parent/ParentDashboard.jsx
+++ src/dashboard/parent/ParentDashboard.jsx
@@ -265,7 +265,7 @@ export default function ParentDashboard({ family, refetch }) {
       <h3 className="fs-2 fw-bold text-center">
         Students Linked With this account
       </h3>
-      <StudentSummaryChart></StudentSummaryChart>
+      {/* <StudentSummaryChart></StudentSummaryChart> */}
       <div className="table-responsive mb-3">
         <table className="table mb-0" style={{ minWidth: 700 }}>
           <thead>
